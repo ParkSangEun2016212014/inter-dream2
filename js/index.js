ad05f49e6597adb6a2ed88b758dc06cd9021d66d
ParkSangEun2016212014 <71581398+ParkSangEun2016212014@users.noreply.github.com>
2022-12-14 18:29:33 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+// inter-dream2
+
+$(setTimeout(function () {
+
+
+    if (!confirm("사소한 거라도 좋아요.\n진짜 꿈이 없어요?")) {
+        //   '아니오'라고 대답
+
+        location.href = "https://parksangeun2016212014.github.io/inter-dream";
+
+
+
+        // '예'라고 대답
+    } else {
+        // dream6으로 이동
+        location.href = "https://parksangeun2016212014.github.io/dream6";
+
+    }
+}, 1000));
+
